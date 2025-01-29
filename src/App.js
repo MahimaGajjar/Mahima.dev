7a42b589763f38a9a33ee1675589306c4b14288e
Mahima Gajjar <88553065+MahimaGajjar@users.noreply.github.com>
2025-01-29 18:41:49 +0530

    Added Navigation to pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Home";
+import ContactMe from "./components/ContactMe";
+import Education from "./components/Education";
+import WorkExperience from "./components/WorkExperience";
+import Projects from "./components/Projects";
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contactme" element={<ContactMe />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/workexperience" element={<WorkExperience />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
