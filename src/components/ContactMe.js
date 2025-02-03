60ec8f99e02bce30ca7b3203ba2bfe08853f3022
Mahima Gajjar <88553065+MahimaGajjar@users.noreply.github.com>
2025-02-04 00:26:33 +0530

    Added the base conetent in Contact Page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,9 +4,43 @@ export const ContactMe = () => {
   return (
     <>
       <div className="container">
-        <div className="heading"></div>
-
-        <div className="form"></div>
+        <div className="heading">
+          <h2>Let's Get in Sync!</h2>
+          <p>Say Hello and Let's Create Something Great!</p>
+        </div>
+        
+        <div className="form">
+          <p>Or Send a message</p>
+          <div className="formcontainer">
+            <form>
+              <p>Name</p>
+              <label>
+                <input type="text" />
+              </label>
+              <label>
+                Email
+                <input type="email" />
+              </label>
+              <br></br>
+              <label>
+                Interested in:
+                <select name="interestedin" required>
+                  <option value="">Interested In</option>
+                  <option value="projects">Projects</option>
+                  <option value="connect">Connect</option>
+                  <option value="other">Other</option>
+                </select>
+              </label>
+              <br />
+              <br />
+              <p>Message</p>
+              <textarea></textarea>
+              <br />
+              <br />
+              <button>Submit</button>
+            </form>
+          </div>
+        </div>
 
         <div className="navigation">
           <a href="/" className="titleContent">
