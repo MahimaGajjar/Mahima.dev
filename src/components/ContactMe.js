01ebd5953161d0a681b349583b6555fe55ff8059
Mahima Gajjar <88553065+MahimaGajjar@users.noreply.github.com>
2025-02-05 01:25:38 +0530

    Changes in Contact me page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -8,35 +8,38 @@ export const ContactMe = () => {
           <h2>Let's Get in Sync!</h2>
           <p>Say Hello and Let's Create Something Great!</p>
         </div>
-        
+
         <div className="form">
-          <p>Or Send a message</p>
           <div className="formcontainer">
+            
+          <p>Or Send a message</p>
             <form>
-              <p>Name</p>
-              <label>
-                <input type="text" />
-              </label>
-              <label>
-                Email
-                <input type="email" />
-              </label>
-              <br></br>
-              <label>
-                Interested in:
+              <div className="row">
+                <div className="input-group">
+                  <label>Name</label>
+                  <input type="text" />
+                </div>
+                <div className="input-group">
+                  <label>Email</label>
+                  <input type="email" />
+                </div>
+              </div>
+
+              <div className="input-group full-width">
+                <label>Interested in:</label>
                 <select name="interestedin" required>
                   <option value="">Interested In</option>
                   <option value="projects">Projects</option>
                   <option value="connect">Connect</option>
                   <option value="other">Other</option>
                 </select>
-              </label>
-              <br />
-              <br />
-              <p>Message</p>
-              <textarea></textarea>
-              <br />
-              <br />
+              </div>
+
+              <div className="input-group full-width">
+                <label>Message</label>
+                <textarea></textarea>
+              </div>
+
               <button>Submit</button>
             </form>
           </div>
