d49871fb613f2fd434b20efcd9ecd42718f964f4
Mahima Gajjar <88553065+MahimaGajjar@users.noreply.github.com>
2025-02-06 17:33:11 +0530

    Minor change

--- src/components/Education.js
+++ src/components/Education.js
@@ -8,7 +8,7 @@ export const Education = () => {
         <div class="card">
           <div class="additional">
             <div class="user-card">
-                <img src="/images/master.png" alt="" />
+              <img src="/images/master.png" alt="" />
               <div class="points center">MSc CSA</div>
             </div>
             <div class="more-info">
@@ -21,13 +21,13 @@ export const Education = () => {
                 <span>Position/Role</span>
                 <span>City, Country</span>
               </div> */}
-             
             </div>
           </div>
           <div class="general">
             <h1>Christ University</h1>
             <p>
-              Masters in Computer Science and Applications from Christ University Banglore. Completed in 2023
+              Masters in Computer Science and Applications from Christ
+              University Banglore. Completed in 2023
             </p>
             <span class="more">Mouse over the card for more info</span>
           </div>
@@ -38,7 +38,7 @@ export const Education = () => {
         <div class="card green">
           <div class="additional">
             <div class="user-card">
-            <img src="/images/bechlore.jpg" alt="" />
+              <img src="/images/bechlore.jpg" alt="" />
               <div class="points center">BSc IT</div>
             </div>
             <div class="more-info">
@@ -47,13 +47,13 @@ export const Education = () => {
                 <span>CGPI</span>
                 <span>8.5/10</span>
               </div>
-
             </div>
           </div>
           <div class="general">
             <h1>Atmiya University</h1>
             <p>
-            Bechlores in Information Technology from Atmiya university, Rajkot Gujarat. Completed in 2021
+              Bechlores in Information Technology from Atmiya university, Rajkot
+              Gujarat. Completed in 2021
             </p>
             <span class="more">Mouse over the card for more info</span>
           </div>
@@ -64,14 +64,14 @@ export const Education = () => {
         <div class="card">
           <div class="additional">
             <div class="user-card">
-            <img src="/images/school.png" alt="school" />
+              <img src="/images/school.png" alt="school" />
 
               <div class="points center">HSC Science</div>
             </div>
             <div class="more-info">
               <h1>School</h1>
               <div class="coords">
-                <span>HSC Science</span> 
+                <span>HSC Science</span>
                 <span>55% </span>
               </div>
               <div class="coords">
@@ -105,7 +105,9 @@ export const Education = () => {
           <div class="general">
             <h1>School</h1>
             <p>
-            I completed my elementary education at Rajkot's Masoom English Medium School. I then finished my education at Krishna English Medium School in Rajkot for my eleventh and twelfth grades.
+              I completed my elementary education at Rajkot's Masoom English
+              Medium School. I then finished my education at Krishna English
+              Medium School in Rajkot for my eleventh and twelfth grades.
             </p>
             <span class="more">Mouse over the card for more info</span>
           </div>
