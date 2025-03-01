cfb0c1eabb3db1fe87b003b7fbd7b2deb9a62956
Mahima Gajjar <88553065+MahimaGajjar@users.noreply.github.com>
2025-03-01 20:36:29 +0530

    Contact Me page responsive

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -58,76 +58,78 @@ export const ContactMe = () => {
   return (
     <>
       <div className="background">
-      <div className="container">
-        <div className="heading">
-          <h2>Let's Get in Sync!</h2>
-          <p>Say Hello and Let's Create Something Great!</p>
-        </div>
+        <div className="container">
+          <div className="heading">
+            <h2>Let's Get in Sync!</h2>
+            <p>Say Hello and Let's Create Something Great!</p>
+          </div>
 
-        <div className="form">
-          <div className="formcontainer">
-            <p>Or Send a message</p>
-            <form onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="input-group">
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                <div className="input-group">
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
+          <div className="form">
+            <div className="formcard">
+              <div className="formcontainer">
+                <p>Or Send a message</p>
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="input-group">
+                      <label>Name</label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                    <div className="input-group">
+                      <label>Email</label>
+                      <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="input-group full-width">
+                    <label>Interested in:</label>
+                    <select
+                      name="interested_in"
+                      value={formData.country}
+                      onChange={handleChange}
+                      required
+                    >
+                      <option value="">Interested In</option>
+                      <option value="projects">Projects</option>
+                      <option value="connect">Connect</option>
+                      <option value="other">Other</option>
+                    </select>
+                  </div>
+
+                  <div className="input-group full-width">
+                    <label>Message</label>
+                    <textarea
+                      name="message"
+                      value={formData.message}
+                      onChange={handleChange}
+                    ></textarea>
+                  </div>
+
+                  <button type="submit">Submit</button>
+                </form>
               </div>
+            </div>
+          </div>
 
-              <div className="input-group full-width">
-                <label>Interested in:</label>
-                <select
-                  name="interested_in"
-                  value={formData.country}
-                  onChange={handleChange}
-                  required
-                >
-                  <option value="">Interested In</option>
-                  <option value="projects">Projects</option>
-                  <option value="connect">Connect</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-
-              <div className="input-group full-width">
-                <label>Message</label>
-                <textarea
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                ></textarea>
-              </div>
-
-              <button type="submit">Submit</button>
-            </form>
+          <div className="navigation">
+            <div className="btn">
+              <a className="btn__link" href="/">
+                <div className="btn__sq"></div>
+              </a>
+            </div>
           </div>
         </div>
-
-        <div className="navigation">
-          <div className="btn">
-            <a className="btn__link" href="/">
-              <div className="btn__sq"></div>
-            </a>
-          </div>
-        </div>
-      </div>
       </div>
     </>
   );
